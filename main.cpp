77fa622010d2d6566d904aa1188a2a9bac30f00c
BelkovskiDaniil <belkadaniil5@gmail.com>
2022-12-25 17:28:39 +0500

    2_1

--- main.cpp
+++ main.cpp
@@ -1,15 +1,20 @@
-/*Дано число N < 106 и последовательность целых чисел из [-231..231] длиной N.
- * Требуется построить бинарное дерево, заданное наивным порядком вставки.
- * Т.е., при добавлении очередного числа K в дерево с корнем root, если root→Key ≤ K,
+/* Дано число N < 106 и последовательность пар целых чисел из [-231, 231] длиной N.
+ * Построить декартово дерево из N узлов, характеризующихся парами чисел (Xi, Yi).
+ * Каждая пара чисел (Xi, Yi) определяет ключ Xi и приоритет Yi в декартовом дереве.
+ * Добавление узла в декартово дерево выполняйте следующим образом:
+ * При добавлении узла (x, y) выполняйте спуск по ключу до узла P с меньшим приоритетом.
+ * Затем разбейте найденное поддерево по ключу x так, чтобы в первом поддереве все ключи меньше x, а во втором больше или равны x.
+ * Получившиеся два дерева сделайте дочерними для нового узла (x, y). Новый узел вставьте на место узла P.
+ * Построить также наивное дерево поиска по ключам Xi.
+ * Т.е., при добавлении очередного числа K в наивное дерево с корнем root, если root→Key ≤ K,
  * то узел K добавляется в правое поддерево root; иначе в левое поддерево root.
- * Выведите элементы в порядке pre-order (сверху вниз).
- * Рекурсия запрещена.*/
+ * Вычислить разницу глубин наивного дерева поиска и декартового дерева.
+ * Разница может быть отрицательна, необходимо вывести модуль разности.*/
 
-//https://contest.yandex.ru/contest/43508/run-report/80023763/
+//https://contest.yandex.ru/contest/43508/run-report/80024323/
 
 #include <iostream>
 #include <vector>
-#include <queue>
 
 struct Node {
     ~Node();
@@ -30,36 +35,40 @@ Node::~Node() {
 class Tree {
 public:
     ~Tree();
-    void add(int key);
-    std::deque<int> pre_order();
+    void Add(int key);
+    // Возвращает true, если успешно удалили
+    bool Remove(int key);
+    int GetDepth() const;
+    void Print() const;
 
 private:
     Node* root = nullptr;
+
+    int GetDepth(Node* node) const;
+    void Print(Node* node, int tabs) const;
 };
 
 Tree::~Tree() {
     delete root;
 }
 
-void Tree::add(int key) {
+void Tree::Add(int key) {
     if (!root) {
         root = new Node(key);
         return;
     }
-
     Node* current = root;
     while (true) {
-
         if (current->Data > key) {
+            // Идем в Left
             if (current->Left != nullptr)
                 current = current->Left;
             else {
                 current->Left = new Node(key, current);
                 break;
             }
-        }
-
-        else {
+        } else {
+            // Идем в Right
             if (current->Right != nullptr)
                 current = current->Right;
             else {
@@ -70,73 +79,157 @@ void Tree::add(int key) {
     }
 }
 
-std::deque<int> Tree::pre_order() {
-    std::deque<int> answer;
-
-    if (root == nullptr){
-        return answer;
+bool Tree::Remove(int key) {
+    Node* current = root;
+    while (current) {
+        if (current->Data < key) current = current->Right;
+        else if (current->Data > key) current = current->Left;
+        else break;
     }
+    if (!current) return false;
 
-    std::deque<Node*> queue_local_left;
-    std::deque<Node*> queue_local_right;
-    queue_local_left.push_back(root);
-    while((!queue_local_left.empty()) or (!queue_local_right.empty())){
-
-        //Сначала проверяем левую ветвь на пустоту
-        if (!queue_local_left.empty()) {
-            Node * node = queue_local_left.front();
-            queue_local_left.pop_front();
-            answer.push_back(node->Data);
-            //Выбираем дальнейший путь
-            if(node->Left != nullptr){
-                queue_local_left.push_front(node->Left);
-            }
-
-            if(node->Right != nullptr){
-                queue_local_right.push_front(node->Right);
-            }
-        }
-
-        //Затем правую
-        else if (!queue_local_right.empty()) {
-            Node * node = queue_local_right.front();
-            queue_local_right.pop_front();
-            answer.push_back(node->Data);
-            //Выбираем дальнейший путь
-            if(node->Left != nullptr){
-                queue_local_left.push_front(node->Left);
-            }
-
-            if(node->Right != nullptr){
-                queue_local_right.push_front(node->Right);
-            }
-        }
-
-
+    if (!current->Right && !current->Left) {
+        if (current == root)
+            root = nullptr;
+        else if (current->Parent->Left == current)
+            current->Parent->Left = nullptr;
+        else
+            current->Parent->Right = nullptr;
+    } else if (current->Right && !current->Left) {
+        if (current == root)
+            root = current->Right;
+        else if (current->Parent->Left == current)
+            current->Parent->Left = current->Right;
+        else
+            current->Parent->Right = current->Right;
+        current->Right->Parent = current->Parent;
+    } else if (!current->Right && current->Left) {
+        if (current == root)
+            root = current->Left;
+        if (current->Parent->Left == current)
+            current->Parent->Left = current->Left;
+        else
+            current->Parent->Right = current->Left;
+        current->Left->Parent = current->Parent;
     }
-    return answer;
+    current->Left = nullptr;
+    current->Right = nullptr;
+    delete current;
+    return true;
+}
+
+//Получаем глубину дерева
+int Tree::GetDepth() const {
+    return GetDepth(root);
+}
+
+//Получаем глубину дерева
+int Tree::GetDepth(Node *node) const {
+    if (!node) return 0;
+    return std::max(GetDepth(node->Left), GetDepth(node->Right)) + 1;
+}
+
+void Tree::Print() const {
+    if (root) Print(root, 0);
+}
+
+void Tree::Print(Node* node, int tabs) const {
+    if (node->Right) Print(node->Right, tabs + 1);
+    for (int i = 0; i < tabs; ++i) std::cout << "\t";
+    std::cout << node->Data << std::endl;
+    if (node->Left) Print(node->Left, tabs + 1);
+}
+
+struct TreapNode {
+    int Key;
+    int Priority;
+    TreapNode* Left = nullptr;
+    TreapNode* Right = nullptr;
+
+    TreapNode(int key, int priority) : Key(key), Priority(priority) {}
+};
+
+class Treap {
+public:
+    void Add(int key, int prioryty);
+    void Print() const { if (root) Print(root, 0); }
+    int GetDepth() const;
+
+private:
+    TreapNode* root = nullptr;
+    std::pair<TreapNode*, TreapNode*> Split(int key, TreapNode* node);
+    TreapNode* Merge(TreapNode* t1, TreapNode* t2);
+
+    int GetDepth(TreapNode* node) const;
+    void Print(TreapNode* node, int tabs) const;
+};
+
+//Сплит дерева
+std::pair<TreapNode*, TreapNode*> Treap::Split(int key, TreapNode* node) {
+    if (!node) return {nullptr, nullptr};
+    if (node->Key < key) {
+        auto [t1, t2] = Split(key, node->Right);
+        node->Right = t1;
+        return {node, t2};
+    } else {
+        auto [t1, t2] = Split(key, node->Left);
+        node->Left = t2;
+        return {t1, node};
+    }
+}
+
+//Функция слияния
+TreapNode* Treap::Merge(TreapNode* left, TreapNode* right) {
+    if (left == nullptr || right == nullptr) {
+        return left == nullptr ? right : left;
+    }
+    if (left->Priority > right->Priority) {
+        left->Right = Merge(left->Right, right);
+        return left;
+    } else {
+        right->Left = Merge(left, right->Left);
+        return right;
+    }
+}
+
+//Добавляем и устанавливаем, исходя из приоритета
+void Treap::Add(int key, int prioryty) {
+    auto [t1, t2] = Split(key, root);
+    int r = prioryty;
+    TreapNode* new_node = new TreapNode(key, r);
+    t1 = Merge(t1, new_node);
+    root = Merge(t1, t2);
+}
+
+//Получаем глубину дерева
+int Treap::GetDepth() const {
+    return GetDepth(root);
+}
+
+//Получаем глубину дерева
+int Treap::GetDepth(TreapNode *node) const {
+    if (!node) return 0;
+    return std::max(GetDepth(node->Left), GetDepth(node->Right)) + 1;
+}
+
+void Treap::Print(TreapNode* node, int tabs) const {
+    if (node->Right) Print(node->Right, tabs + 1);
+    for (int i = 0; i < tabs; ++i) std::cout << "\t";
+    std::cout << node->Key << std::endl;
+    if (node->Left) Print(node->Left, tabs + 1);
 }
 
 int main() {
+    Treap treap;
     Tree tree;
-    int n = 0, number = 0;
+    int n = 0, a = 0, b = 0;
     std::cin >> n;
-
-    for (int i = 0; i < n; i++) {
-        std::cin >> number;
-        tree.add(number);
+    for(int i = 0; i < n; i++){
+        std::cin >> a >> b;
+        tree.Add(a);
+        treap.Add(a, b);
     }
 
-    //Заполняем очередь
-    std::deque<int> answer = tree.pre_order();
-
-    //Печатаем, пока очередь не опустеет
-    while(!answer.empty()){
-        std::cout << answer.front() << ' ';
-        answer.pop_front();
-    }
-
-    std::cout << std::endl;
-
+    std::cout << abs(tree.GetDepth() - treap.GetDepth());
     return 0;
 }
