c40862a39169f3421e4b2a51c754a7ee7d847066
BelkovskiDaniil <belkadaniil5@gmail.com>
2023-03-12 22:07:45 +0500

    2_2

--- Second_sem_first_homework/local_file.cpp
+++ Second_sem_first_homework/local_file.cpp
@@ -1,34 +1,35 @@
-/*В дощечке в один ряд вбиты гвоздики.
- * Любые два гвоздика можно соединить ниточкой.
- * Требуется соединить некоторые пары гвоздиков ниточками так, чтобы к каждому
- * гвоздику была привязана хотя бы одна ниточка, а суммарная длина всех ниточек была минимальна.
+/*Дано N кубиков. Требуется определить каким количеством способов
+ *можно выстроить из этих кубиков пирамиду.
+ *Каждый вышележащий слой пирамиды должен быть не больше нижележащего.
  *
- * https://contest.yandex.ru/contest/46386/run-report/83838568/
+ * https://contest.yandex.ru/contest/46386/run-report/83839374/
  */
 
 #include <iostream>
-#include "algorithm"
+#include <vector>
 
-int main() {
-    int n, number;
-    std::cin >> n;
-    int *array = new int[n];
-    int *answer_array = new int[n];
-    for (int i = 0; i < n; i++) {
-        std::cin >> number;
-        array[i] = number;
-    }
-    for (int i = 0; i < n; i++) {
-        answer_array[i] = 0;
-    }
-    std::vector<int> vector (array, array + n);
-    sort (vector.begin(), vector.end());
-    answer_array[1] = vector[1] - vector[0];
-    if (n > 2) {
-        answer_array[2] = vector[2] - vector[0];
-        for (int i = 3; i < n; i++) {
-            answer_array[i] = std::min(answer_array[i - 2], answer_array[i - 1]) + vector[i] - vector[i - 1];
+long long find_forms(int n, std::vector<std::vector<long long>>& answer_vector) {
+    //Рассматриваем возможные формы, ходя по "столбцам" и "строкам"
+    for (int i = 0; i < n + 1; i++) {
+        for (int j = 2; j < n + 1; j++) {
+            answer_vector[i][j] = answer_vector[i][j - 1];
+            if (0 < i - j + 1){
+                answer_vector[i][j] += answer_vector[i - j][j];
+            }
         }
     }
-    std::cout << answer_array[n - 1];
+    return answer_vector[n][n];
+}
+
+int main() {
+    int n = 0;
+    std::cin >> n;
+    std::vector<std::vector<long long>> vector_local(n + 1, std::vector<long long>(n + 1));
+    //Заполняем первые два столбца таблицы
+    for (int i = 0; i < n + 1; i++) {
+        vector_local[i][0] = 0;
+        vector_local[i][1] = 1;
+    }
+    std::cout << find_forms(n, vector_local) << std::endl;
+    return 0;
 }
