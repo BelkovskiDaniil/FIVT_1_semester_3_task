c5e004eadfdf95c33b221f71f6312a6559f6a0c5
BelkovskiDaniil <belkadaniil5@gmail.com>
2023-03-12 22:21:07 +0500

    3_2

--- Second_sem_first_homework/local_file.cpp
+++ Second_sem_first_homework/local_file.cpp
@@ -1,52 +1,43 @@
-/*Дано невзвешенное дерево. Расстоянием между двумя вершинами будем
- *называть количество ребер в пути, соединяющем эти две вершины.
- *Для каждой вершины определите сумму расстояний до всех остальных вершин.
- *Время работы должно быть O(n).
+/*Даны два "длинных" неотрицательных числа. Требуется найти результат их сложения.
  *
- *https://contest.yandex.ru/contest/46386/run-report/83843069/
+ *https://contest.yandex.ru/contest/46386/run-report/83845057/
  */
 
 #include <iostream>
-#include <vector>
-
-
-long long int find_distance(int i, std::vector<int>& counter, std::vector<std::vector<int>>& vector_local) {
-    int distance = 0;
-    for (int element : vector_local[i]) {
-        distance += find_distance(element, counter, vector_local) + counter[element];
-        counter[i] += counter[element];
-    }
-    counter[i] += 1;
-    return distance;
-}
+#include <string>
+#include <algorithm>
+#include <cmath>
 
 int main() {
-    int number, first, second;
-    std::cin >> number;
-    std::vector<std::vector<int>> vector_local(number);
-    std::vector<int> prev(number);
+    std::string biggest, smallest, str, first_number, second_number, answer, answer_new;
+    int mem = 0, number, sum, end;
+    std::cin >> first_number;
+    std::cin >> second_number;
 
-    //Распределяем вершины и корни ребер
-    for (int i = 0; i < number - 1; i++) {
-        std::cin >> first;
-        std::cin >> second;
-        if (first > second)
-            std::swap(first, second);
-        vector_local[first].push_back(second);
-        prev[second] = first;
+    //Находим наибольшее число
+    if (first_number.size() >= second_number.size()) {
+        biggest = first_number;
+        smallest = second_number;
+    }
+    else {
+        biggest = second_number;
+        smallest = first_number;
     }
 
-    std::vector<int> counter (number, 0);
-
-    //Находим расстояние
-    std::vector<long long int> dist(number);
-    dist[0] = find_distance(0, counter, vector_local);
-    std::cout << dist[0] << std::endl;
-
-    for (int v = 1; v < number; ++v) {
-        dist[v] = dist[prev[v]] - counter[v] + (number - counter[v]);
-        std::cout << dist[v] << std::endl;
+    //Дописываем нули меньшему числу и складываем столбиком
+    number = abs(biggest.size() - smallest.size());
+    for (int i = 0; i < number; i++) smallest = '0' + smallest;
+    for (int i = biggest.size() - 1; i >= 0; i--) {
+        sum = (biggest[i] - '0') + (smallest[i] - '0') + mem;
+        end = sum % 10;
+        str += end + '0';
+        if (i == 0) str += std::to_string(sum);
+        mem = sum / 10;
     }
-
+    reverse(str.begin(), str.end());
+    answer = str.erase(0, 1);
+    if (answer.length() >= 2 && answer[0] == '-' && answer[1] == '-') answer_new = answer.substr(1, answer.length() - 1);
+    else answer_new = answer;
+    std::cout << answer_new << std::endl;
     return 0;
 }
